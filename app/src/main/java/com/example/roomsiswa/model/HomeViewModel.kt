c1e703ea6c33a0eaa90305d19d21fd9790fb5984
TC1005 <curvafans383@gmail.com>
2023-12-06 21:55:38 +0700

    Menambahkan Class Homeviewmodel serta mengisi repositorisiswa

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
